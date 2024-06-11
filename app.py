8656a74d47fd3550ae77ad6fc93478ac96e7a39c
Juraj Borza <jborza@gmail.com>
2024-06-11 19:29:09 +0200

    flask templates

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/user/<username>')
+def user(username):
+    return 'User %s' % username
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('hello.html', name=name)
+
+@app.route('/users')
+def users():
+    user_names = ['Alice', 'Bob', 'Charlie']
+    return render_template('users.html', names=user_names)
