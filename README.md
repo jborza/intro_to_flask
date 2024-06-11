2a06473cc8bf5314e7f89659dcb6aac963fe0a3d
Juraj Borza <jborza@gmail.com>
2024-06-11 20:57:17 +0200

    storing a form (flaskform) data into the database

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # intro_to_flask
  
+uses packages:
+flask
+flask-sqlalchemy
+flask-migrate
+flask-WTF
--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField
+from wtforms.validators import InputRequired, Length
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
@@ -14,6 +17,19 @@ class Task(db.Model):
     description = db.Column(db.String(200), nullable=True)
     is_completed = db.Column(db.Boolean, default=False)
     done = db.Column(db.Boolean, default=False)
+
+class TaskForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired(), Length(min=1, max=100)])
+    description = TextAreaField('Description', validators=[Length(max=200)])
+    is_complete = BooleanField('Is Completed')
+    submit = SubmitField('Submit')
+
+@app.route('/task', methods=['GET', 'POST'])
+def task():
+    form = TaskForm()
+    if form.validate_on_submit():
+        return redirect(url_for('hello_world'))
+    return render_template('task.html', form=form)
 
 @app.route('/')
 def hello_world():
