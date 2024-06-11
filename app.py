3a8cc745d5ecefbe1f321b771c736346e91ebb32
Juraj Borza <jborza@gmail.com>
2024-06-11 20:44:11 +0200

    db creation

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+app.config['SECRET_KEY'] = 'c41b428eee11a1fe001808ed3d3fa29ed2a23f7175552395'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    is_completed = db.Column(db.Boolean, default=False)
+    done = db.Column(db.Boolean, default=False)
 
 @app.route('/')
 def hello_world():
